ff048e62f6438e7b016612cbc990efe4a6a5fecd
Md Tauqeer <mdtauqeer525217@gmail.com>
2024-03-13 21:00:29 +0530

    make the link active for moving to various pages

--- clientside/src/main.jsx
+++ clientside/src/main.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
+import 'bootstrap/dist/css/bootstrap.min.css'
 import Home from './pages/Home.jsx';
 import About from './pages/About.jsx'
 import Contact from './pages/contact.jsx'
