807a59cf90d2b1a0d33ced47284d9fc1744036b2
Md Tauqeer <mdtauqeer525217@gmail.com>
2024-03-13 20:52:24 +0530

    header nav bar design

--- clientside/src/components/layout/Header.jsx
+++ clientside/src/components/layout/Header.jsx
@@ -1,34 +1,34 @@
 import React from 'react'
-import styles from'./header.module.css'
 import {Link} from 'react-router-dom'
+import { TiShoppingCart } from "react-icons/ti";
 
 function Header () {
   return (
     <>
-   <nav className="navbar navbar-expand-lg navbar-light bg-light" >
+<nav className="navbar navbar-expand-lg navbar-light bg-light" >
   <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarTogglerDemo01" aria-controls="navbarTogglerDemo01" aria-expanded="false" aria-label="Toggle navigation">
     <span className="navbar-toggler-icon"></span>
   </button>
   <div className="collapse navbar-collapse" id="navbarTogglerDemo01">
-    <Link className="navbar-brand" to="#">E-commerce</Link>
+    <Link className="navbar-brand nav-heading" to="#"><TiShoppingCart className="nav-head-icon" />ECOMMERCE APP</Link>
     <ul className="navbar-nav ms-auto mt-2 mt-lg-0">
-      <li className="nav-item active">
-        <Link  className="nav-link" to="/">Home </Link>
+      <li className="nav-item ">
+        <Link  className="nav-link page-link" to="/">Home</Link>
       </li>
       <li className="nav-item">
-        <Link  className="nav-link" to="/sign-up">Signup</Link>
+        <Link   className="nav-link page-link" to="#">Category</Link>
+      </li>
+      <li className="nav-item">
+        <Link  className="nav-link page-link" to="/register">Register</Link>
       </li>
       
       <li className="nav-item">
-        <Link  className="nav-link" to="login">Login</Link>
+        <Link  className="nav-link page-link" to="/login">Login</Link>
       </li>
       <li className="nav-item">
-        <Link  className="nav-link" to="/Cartpage">Cart(0)</Link>
+        <Link  className="nav-link page-link" to="/Cartpage">Cart(0)</Link>
       </li>
-
-
-    </ul>
-    
+    </ul>   
   </div>
 </nav>
     </>
--- clientside/src/main.jsx
+++ clientside/src/main.jsx
@@ -1,7 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
-import 'bootstrap/dist/css/bootstrap.min.css'
 import Home from './pages/Home.jsx';
 import About from './pages/About.jsx'
 import Contact from './pages/contact.jsx'
