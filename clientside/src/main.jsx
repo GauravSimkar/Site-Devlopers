554881f03c1f724fc3cf3b6270ce4502ebdc1dd0
talha <talha384672@gmail.com>
2024-03-14 15:02:05 +0530

    issue resolved

--- clientside/src/main.jsx
+++ clientside/src/main.jsx
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import Home from './pages/Home.jsx';
 import Register from './pages/Auth/Register.jsx'
-import Login from './pages/Auth/login.jsx'
+// import Login from './pages/Auth/login.jsx'
 import Aboutus from './components/Aboutus.jsx'
 import Contactus from './components/Contactus.jsx'
 import CartPages from './pages/CartPages.jsx'
@@ -18,7 +18,7 @@ const router=createBrowserRouter([
     children:[
     {path:'/' , element:<Home/>,},
     {path:'/register', element:<Register/>},
-    {path:'/login',element:<Login/>},
+    // {path:'/login',element:<Login/>},
     {path:'/Aboutus',element:<Aboutus/>},
     {path:'/Contactus',element:<Contactus/>},
     {path:'/Cartpage',element:<CartPages/>},
