8df7790842d0a46c42b177d0f94f78237676271f
Md Tauqeer <mdtauqeer525217@gmail.com>
2024-03-15 00:48:18 +0530

    making context api

---
+++ clientside/src/components/contextAPI/Authcontext.jsx
@@ -0,0 +1,26 @@
+import { createContext,useEffect,useState} from "react";
+
+export const Authcontext=createContext({
+   user:null,
+   token:""
+});
+
+const AuthcontextProvider=({children})=>{
+   const [auth,setauth]=useState(Authcontext);
+   useEffect(()=>{
+     const data=JSON.parse(localStorage.getItem('auth'));
+     if(data){
+     setauth({...auth,
+        user:data.user,
+        token:data.token
+     });
+     //eslint-disable-next-line
+    }},[]);
+return(
+    <Authcontext.Provider value={[auth,setauth]}>
+        {children}
+    </Authcontext.Provider>
+);  
+}
+
+export default AuthcontextProvider;
