6619114c5d2f0329dfbbc05fe2466f2fa885a7fb
Md Tauqeer <mdtauqeer525217@gmail.com>
2024-03-15 11:38:12 +0530

    creating dashboard page

---
+++ clientside/src/pages/user/Dashboard.jsx
@@ -0,0 +1,10 @@
+import Layout from "../../components/layout/layout.jsx";
+
+const Dashboard=()=>{
+    return(
+        <Layout>
+            Dashboard
+        </Layout>
+    ); 
+}
+export default Dashboard;
